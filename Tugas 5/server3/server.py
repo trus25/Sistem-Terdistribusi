3ff6b04ee96420d080828c44baff29def4b7cc15
Farhan Zuhdi <32932072+trus25@users.noreply.github.com>
2019-11-06 21:29:29 +0700

    update

--- Tugas 5/server3/server.py
+++ Tugas 5/server3/server.py
@@ -1,10 +1,16 @@
-import sys
-sys.path.append("..")
-
-from clients.backend import *
+from fileserver import  *
 import Pyro4
+import sys
 
-namainstance = "fileserver2"
+namainstance = sys.argv[1] or "fileserver"
+
+def start_without_ns():
+    daemon = Pyro4.Daemon()
+    x_FileServer = Pyro4.expose(FileServer)
+    uri = daemon.register(x_FileServer)
+    print("my URI : ", uri)
+    daemon.requestLoop()
+
 
 def start_with_ns():
     #name server harus di start dulu dengan  pyro4-ns -n localhost -p 7777
@@ -13,11 +19,12 @@ def start_with_ns():
 
     daemon = Pyro4.Daemon(host="localhost")
     ns = Pyro4.locateNS("localhost",7777)
-    x_FileServer = Pyro4.expose(Backend)
+    x_FileServer = Pyro4.expose(FileServer)
     uri_fileserver = daemon.register(x_FileServer)
-    #ns.register("{}" . format(namainstance), uri_fileserver)
+    ns.register("{}" . format(namainstance), uri_fileserver)
     #untuk instance yang berbeda, namailah fileserver dengan angka
-    ns.register(namainstance, uri_fileserver)
+    #ns.register("fileserver2", uri_fileserver)
+    #ns.register("fileserver3", uri_fileserver)
     daemon.requestLoop()
 
 
--- tugas 5/fileserver.py
+++ tugas 5/fileserver.py
@@ -3,15 +3,7 @@ import base64
 
 class FileServer(object):
     def __init__(self):
-        self.pyro_server = ["fileserver1","fileserver2","fileserver3"]
-        self.pyro = dict()
-
-    def pyro_connect(self):
-        i = 0
-        for list in self.pyro_server:
-            uri = "PYRONAME:{}@localhost:7777" . format(list)
-            self.pyro[i] = Pyro4.Proxy(uri)
-            i+=1
+        pass
 
     def create_return_message(self,kode='000',message='kosong',data=None):
         return dict(kode=kode,message=message,data=data)
@@ -28,8 +20,6 @@ class FileServer(object):
             return self.create_return_message('500','Error')
 
     def create(self, name='filename000'):
-        for x in range(0, len(self.pyro)):
-            self.pyro[x].create(name, isi)
         nama='FFF-{}' . format(name)
         print("create ops {}" . format(nama))
         try:
@@ -51,9 +41,6 @@ class FileServer(object):
         except:
             return self.create_return_message('500','Error')
     def update(self,name='filename000',content=''):
-        for x in range(0, len(self.pyro)):
-            self.pyro[x].update(name, content)
-
         nama='FFF-{}' . format(name)
         print("update ops {}" . format(nama))
 
@@ -68,10 +55,6 @@ class FileServer(object):
             return self.create_return_message('500','Error',str(e))
 
     def delete(self,name='filename000'):
-        for x in range(0, len(self.pyro)):
-            flag += 1
-            self.pyro[x].delete(name)
-            
         nama='FFF-{}' . format(name)
         print("delete ops {}" . format(nama))
 
